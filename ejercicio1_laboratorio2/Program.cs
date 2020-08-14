7443653dc7cef1a42731e53400081f384fdb2973
Geronimo Schmidt <geroschmidt@hotmail.com>
2020-08-14 19:40:00 -0300

    primer commit

---
+++ ejercicio1_laboratorio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1_laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Mostrar numero al azar del 1 al 100
+
+            int miliseg = DateTime.Now.Millisecond;
+            int dado = miliseg % 100 + 1;
+            Console.WriteLine("El numero del dado es " + dado);
+            Console.ReadKey();
+        }
+    }
+}
